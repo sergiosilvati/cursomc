b13c0f7072bc9ff95edcc6fff8c9548e623848a9
Sergio Silva <sergiosilva.ti@hotmail.com>
2018-11-09 10:11:42 -0200

    Produto e associação muitos para muitos

--- src/main/java/com/sergio/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/sergio/cursomc/repositories/ProdutoRepository.java
@@ -3,10 +3,10 @@ package com.sergio.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sergio.cursomc.domain.Categoria;
+import com.sergio.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
 	
 
--- src/main/java/com/sergio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sergio/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,7 @@
 package com.sergio.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
